c6d4c05b4923c4df28787bf96d549c699e83e70f
Lucas Torri <lucastorri@gmail.com>
2017-06-21 19:12:38 +0200

    remove unused imports

--- macros/src/main/scala/com/unstablebuild/settler/model/MemorySize.scala
+++ macros/src/main/scala/com/unstablebuild/settler/model/MemorySize.scala
@@ -1,10 +1,5 @@
 package com.unstablebuild.settler.model
 
-import java.time.{Duration => JavaDuration}
-
-import scala.language.experimental.macros
-
-
 case class MemorySize(toBytes: Long) extends Ordered[MemorySize] {
 
   override def compare(that: MemorySize): Int = toBytes.compareTo(that.toBytes)
